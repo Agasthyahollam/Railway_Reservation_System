56d6b6213f8a9c9eca64735a72ccf590f9a8a07a
Agasthya Holla M <52572604+Agasthyahollam@users.noreply.github.com>
2020-04-01 19:33:23 +0530

    Add files via upload

--- passwordcheck.c
+++ passwordcheck.c
@@ -1,28 +1,29 @@
+
 #include<stdio.h>
-
 #include<string.h>
+#include<stdlib.h>
+int my_strcmp(char str1[], char str2[]);
+void my_strcpy(char dest[], char src[]);
+void pass();
+int  user();
+int phno1();
+char name[30], pwd[25], ver[25], usn[30], phno[15];
+char com[10] = ".com"; char check[20];
+int i, a, digi, up, low, schar, s;
 
-void main()
 
+
+
+void pass()
 {
-
-	char name[25], pwd[25], ver[25];
-
-	int i, a, digi , up , low , schar ;
-
-	printf("Enter your Name: ");
-
-	gets(name);
 renter:
-	digi = 0; 
-	up = 0; 
-	low = 0; 
+	digi = 0;
+	up = 0;
+	low = 0;
 	schar = 0;
-
- printf("Hello %s, Enter your password:\n", name);
-
-	gets(pwd);
-
+	
+	printf("PASSWORD RULES :\n *Password should consist of minimum 5 characters\n *Maximum number of characters are 12\n *Password should consist of atleast 1 upper case and atleast 1 lowercase character\n *Password should consist of atleast 1 spl character ('@','#','$')\n *Password should consist of atleast 1 digit\n");
+	scanf("%s", pwd);
 	a = strlen(pwd);
 
 	if (a < 5)
@@ -45,7 +46,7 @@ renter:
 
 	{
 
-		for (i = 0; pwd[i] != NULL; i++)
+		for (i = 0; pwd[i] !='\0'; i++)
 
 		{
 
@@ -71,40 +72,42 @@ renter:
 
 	if (up == 0)
 	{
-		printf("\nThere must be at least one Uppercase\n"); 
+		printf("\nThere must be at least one Uppercase\n");
 
 
-			goto renter; 
+		goto renter;
 	}
 
 	if (low == 0)
 	{
-		printf("\nThere must be at least one Lowercase\n"); 
-		goto renter; 
-		
+		printf("\nThere must be at least one Lowercase\n");
+		goto renter;
+
 	}
-     if (digi == 0)
+	if (digi == 0)
 	{
-		printf("\nThere must be at least one Digit\n"); 
-		goto renter; 
-	 }
-	 if (schar == 0)
-	{ 
-		printf("\nThere must be at least one Special Character\n"); 
-		goto renter; 
+		printf("\nThere must be at least one Digit\n");
+		goto renter;
+	}
+	if (schar == 0)
+	{
+		printf("\nThere must be at least one Special Character\n");
+		goto renter;
 	}
 	else
-    {  renter1:
+	{
+	renter1:
 		printf("Confirm your Password\n");
 
-		gets(ver);
+		scanf("%s",ver);
 
-		if (strcmp(pwd, ver) == 0) 
+
+		if (strcmp(pwd, ver) == 0)
 		{
 
 			printf("Welcome %s, your is Password Verified", name);
 		}
-		else 
+		else
 		{
 
 			printf("Password did not match\n");
@@ -112,4 +115,140 @@ renter:
 		}
 	}
 
-	getch(); }
+	
+}
+int  user()
+{
+return3:
+	printf("Hello %s,Enter your Username(Valid Email ID):\n", name);
+
+	schar = 0;
+
+	scanf("%s", usn);
+
+	//a = strlen(usn);
+	for (int j = 0; usn[j] != '\0'; j++)
+	{
+		if (usn[j] == '@')
+			schar++;
+		if (usn[j] >= 'A' && usn[j] <= 'Z')
+			up++;
+	}//printf("%d", schar);
+	//printf("a=%d", a);
+
+	my_strcpy(check, usn);
+	s = my_strcmp(check, com);
+
+
+	//printf("%d", s);
+	if (s == 0 && schar == 1 && up == 0)
+
+	{
+		printf("Valid EMAIL/USERNAME\n");
+		return 1;
+	}
+	else
+	{
+		printf("Invalid Username\n");
+		goto return3;
+
+	}
+
+}
+void my_strcpy(char dest[], char src[])
+{
+	int count;
+	int z;
+	z = strlen(src);
+	for (count = (z - 4); src[count] != '\0'; count++)
+		dest[count] = src[count];
+	dest[count] = '\0';
+}
+int   reverse_str(char sub_str[])
+{
+	char temp_str[60];
+	int s_len = strlen(sub_str);
+	int i;
+
+	for (i = 0; i < s_len; i++)
+		temp_str[s_len - (i + 1)] = sub_str[i];
+	temp_str[s_len] = '\0';
+
+	for (i = 0; i < s_len; i++)
+		sub_str[i] = temp_str[i];
+	sub_str[s_len] = '\0';
+	return 0;
+}
+
+int my_strcmp(char str1[], char str2[])
+{
+	int len1 = strlen(str1);	// Use the function written earlier to find the string length
+	int len2 = strlen(str2);
+	int ret_val = 1, count;
+
+	if (len1 == len2)	// Proceed only if the two strings are of equal length
+	{
+		for (count = 0; count < len1; count++)
+			if (str1[count] != str2[count])
+			{
+				ret_val = 0;
+				break;
+			}
+	}
+	else
+		ret_val = 0;
+	return ret_val;
+}
+
+int phno1()
+{
+	int count = 0; int i;
+
+	for (i = 0; phno[i] != '\0'; i++)
+	{
+		count++;
+	}
+	if (count == 10)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+
+
+void main()
+
+{
+	int k; int y;
+	printf("Enter your Name: ");
+
+
+	scanf("%s",name);
+system("clear");
+	k = user();
+	if (k == 1)
+	{
+		pass();
+	}
+system("clear");
+return4:
+	printf("\nPls enter your Contact No\n");
+	scanf("%s",phno);
+	y = phno1();
+	if (y == 0)
+		printf("\nSUCESSFULLY REGISTERED");
+	else
+	{
+		printf("\nInvalid phone number\n");
+		goto return4;
+	}
+	//printf("check=%s", check);
+}
+
+
+		
+	
